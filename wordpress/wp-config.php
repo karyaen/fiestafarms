9acd83bd58911320906ddcfc621e3d8fdb7e6083
Lionel Mann <lionelmann@gmail.com>
2013-06-20 17:08:54 -0400

    added gitignore

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -33,7 +33,7 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 define('WP_HOME','http://fiesta.dev');
-define('WP_SITEURL','http://fiesta.dev/wordpress');
+define('WP_SITEURL','http://fiesta.dev/wordpress/');
 //define('WP_DEBUG', true);
 
 /**#@+
